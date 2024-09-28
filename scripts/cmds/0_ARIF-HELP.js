13fa51a590c20fe954545b3814aed134933df3d3
ARIF-PROJECT <projectarif@yandex.com>
2024-09-28 15:20:29 +0530

    Update 0_ARIF-HELP.js

--- scripts/cmds/0_ARIF-HELP.js
+++ scripts/cmds/0_ARIF-HELP.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ ♕︎ | QueenBot V2 ]";
+const doNotDelete = "[ 🥀♥️ | ARIF BABU ]";
 /**
 * @author NTKhang
 * @author: do not delete it
