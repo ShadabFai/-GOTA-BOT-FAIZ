4b190a7835a49a8855c85f90a329e04e19826c16
ARIF-PROJECT <projectarif@yandex.com>
2024-09-30 11:41:03 +0530

    Update 0_ARIF-GROUP-INF.js

--- scripts/cmds/0_ARIF-GROUP-INF.js
+++ scripts/cmds/0_ARIF-GROUP-INF.js
@@ -52,7 +52,7 @@ const name = infu[qtv2[i].id].name;
       var callback = () =>
         api.sendMessage(
           {
-            body: `🔧「 𝐆𝐂 𝐍𝐚𝐦𝐞 」:${threadName}\n🔧「 𝐆𝐫𝐨𝐮𝐩 𝐈𝐃 」: ${id}\n🔧「 𝐀𝐩𝐩𝐫𝐨𝐯𝐚𝐥 」: ${pd}\n🔧「 𝐄𝐦𝐨𝐣𝐢 」: ${icon}\n🔧「 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 」: 𝐈𝐧𝐜𝐥𝐮𝐝𝐢𝐧𝐠 ${threadMem} 𝐌𝐞𝐦𝐛𝐞𝐫𝐬\n🔧「 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐚𝐥𝐞𝐬 」: ${nam}\n🔧「 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐅𝐞𝐦𝐚𝐥𝐞𝐬 」:  ${nu}\n🔧「 𝐓𝐨𝐭𝐚𝐥 𝐀𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐭𝐨𝐫𝐬 」: ${qtv} \n「 𝐈𝐧𝐜𝐥𝐮𝐝𝐞 」:\n${listad}\n🔧「 𝐓𝐨𝐭𝐚𝐥 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐞𝐬𝐬𝐚𝐠𝐞𝐬 」: ${sl} msgs.\n\n𝐌𝐚𝐝𝐞 𝐖𝐢𝐭𝐡 ❤️ 𝐁𝐲: TERAA BAPPP 🙂`,
+            body: `╭•┄┅═══❁🌺❁═══┅┄•╮\n   आरिफ़-बाबू!!🖤\n╰•┄┅═══❁🌺❁═══┅┄•╯ \n\n  🌺  [ 1 ]  𒁍 BOT NAME   → ${threadName}\n🌺  [ 2 ]  𒁍 GROUP NAME   → ${id}\n🌺  [ 3 ]  𒁍 APPROVAL   → ${pd}\n🌺  [ 4 ]  𒁍 EMOJI   → ${icon}\n🌺  [ 5 ]  𒁍  MEMBERS  → ${threadMem}\n🌺  [ 6 ]  𒁍  MALES  → ${nam}\n🌺  [ 7 ]  𒁍  FEMALES  →  ${nu}\n🌺  [ 8 ]  𒁍  TOTAL ADMIN  → ${qtv} \n╭•┄┅═══❁🌺❁═══┅┄•╮\n  🌸   𝐀𝐑𝐈𝐅-𝐁𝐀𝐁𝐔  🌸\n╰•┄┅═══❁🌺❁═══┅┄•╯`,
             attachment: fs.createReadStream(__dirname + '/cache/1.jpg')
           },
           event.threadID,
