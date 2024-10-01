f8dd8ad51a601f2e31eff83c6fc4b428820dc56a
GITHUB78600 <githup3@yandex.com>
2024-10-01 19:50:26 +0530

    Create 0_ARIF-KHANA.js

---
+++ scripts/cmds/0_ARIF-KHANA.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+   name: "khana",
+   version: "1.0",
+   author: "MR.ARIF",
+   countDown: 5,
+   role: 0,
+   shortDescription: "no prefix",
+   longDescription: "no prefix",
+   category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "khana") {
+ return message.reply({
+ body: "𝐘𝐀 𝐋𝐎 𝐁𝐀𝐁𝐔 𝐊𝐇𝐀𝐍𝐀 𝐊𝐇𝐀 𝐋𝐎 😁",
+ attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/qvrtCL00/KHANA.gif")
+ });
+ }
+ }
+}
