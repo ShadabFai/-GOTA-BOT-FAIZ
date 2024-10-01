f4ca58935107b64ed553bc8e6d42c8170831422a
GITHUB78600 <githup3@yandex.com>
2024-10-01 19:56:20 +0530

    Create 0_ARIF-KISS-YOU.js

---
+++ scripts/cmds/0_ARIF-KISS-YOU.js
@@ -0,0 +1,21 @@
+module.exports = {
+ config: {
+   name: "kiss you",
+   version: "1.0",
+   author: "MR.ARIF",
+   countDown: 5,
+   role: 0,
+   shortDescription: "no prefix",
+   longDescription: "no prefix",
+   category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "kiss you") {
+ return message.reply({
+ body: "𝑰 𝑲𝑰𝑺𝑺 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑩𝑨𝑩𝒀 💋🙊💞",
+ attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/65GftY6h/KISS-YOU.gif")
+ });
+ }
+ }
