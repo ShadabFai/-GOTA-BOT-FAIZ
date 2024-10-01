7982adb502694803b8da7d92eb12477b28b069ed
GITHUB78600 <githup3@yandex.com>
2024-10-01 20:08:08 +0530

    Create 0_ARIF-NACHO.js

---
+++ scripts/cmds/0_ARIF-NACHO.js
@@ -0,0 +1,22 @@
+module.exports = {
+ config: {
+   name: "Nacho",
+   version: "1.0",
+   author: "MR.ARIF",
+   countDown: 5,
+   role: 0,
+   shortDescription: "no prefix",
+   longDescription: "no prefix",
+   category: "no prefix",
+ },
+
+ onStart: async function(){}, 
+ onChat: async function({ event, message, getLang }) {
+ if (event.body && event.body.toLowerCase() === "Nacho") {
+ return message.reply({
+ body: "𝐍𝐀𝐂𝐇𝐎 𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍 𝐍𝐀𝐂𝐇𝐎 🙂🤟 ❜",
+ attachment: await global.utils.getStreamFromURL("https://i.postimg.cc/sg9vsZkX/DANCE.gif")
+ });
+ }
+ }
+}
